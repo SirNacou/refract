a8ceb42ee9683eb66d22c2f408b73442280a04d5
nacou <quocanhthedev@gmail.com>
2026-01-09 10:23:29 +0700

    chore: clear codebase for rebuild v2.0

--- services/redirector/src/models/url.rs
+++
@@ -1,9 +0,0 @@
-use chrono::{DateTime, Utc};
-use sqlx::FromRow;
-
-#[derive(Debug, Clone, FromRow)]
-pub struct UrlRecord {
-    pub original_url: String,
-    pub expires_at: DateTime<Utc>,
-    pub is_active: bool,
-}
