e894d817f00b9c2c96274cd017e933f4970f257f
solus <quocanhthedev@gmail.com>
2026-01-26 00:31:55 +0700

    feat: add demo routes for server functions, SSR, and table with TanStack Query
    - Implemented a Todo example using server functions in `start.server-funcs.tsx`
    - Created SSR routes for data-only and full SSR in `start.ssr.data-only.tsx` and `start.ssr.full-ssr.tsx`
    - Added an index route for SSR demos in `start.ssr.index.tsx`
    - Developed a SPA mode for punk songs in `start.ssr.spa-mode.tsx`
    - Introduced a table demo with filtering and sorting capabilities in `table.tsx`
    - Integrated TanStack Query for managing todos in `tanstack-query.tsx`
    - Established a main index route with feature highlights in `index.tsx`
    - Added global styles in `styles.css` and configured TypeScript in `tsconfig.json`
    - Set up Vite configuration with necessary plugins in `vite.config.ts`

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,23 @@
+import { db } from '@/db'
+import { env } from '@/env'
+import * as authSchema from 'auth-schema.ts'
+import { betterAuth } from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+    schema: authSchema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }
+  },
+  plugins: [tanstackStartCookies()],
+})
